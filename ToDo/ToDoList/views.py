c63a498250541fe7466da7db49d21a2d0301537c
Tim Slivensky <tslivensky@emailatg.com>
2020-03-13 10:47:49 -0500

    working

--- ToDo/ToDoList/views.py
+++ ToDo/ToDoList/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import List
 
 # Create your views here.
+
+def home(request):
+	allItems = List.objects.all
+	
+	return render(request, 'home.html', {'all_items': allItems})
+	
+def about(request):
+	return render(request, 'about.html', {})
